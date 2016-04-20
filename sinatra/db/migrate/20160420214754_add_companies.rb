ce4f1ef50a5d28016226c770b3284651579a40e3
nbicode <nbi.decode@gmail.com>
2016-04-20 23:38:47 +0000

    tables created

--- sinatra/db/migrate/20160420214754_add_companies.rb
+++
@@ -1,4 +0,0 @@
-class AddCompanies < ActiveRecord::Migration
-  def change
-  end
-end
--- sinatra/db/schema.rb
+++ sinatra/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,11 +11,74 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321144528) do
+ActiveRecord::Schema.define(version: 20160420233557) do
 
-  create_table "users", force: true do |t|
-    t.string "name"
-    t.string "email"
+  create_table "companies", force: :cascade do |t|
+    t.string  "company_name"
+    t.string  "url"
+    t.date    "created_at"
+    t.date    "updated_at"
+    t.integer "location_id"
+    t.integer "company_profile_id"
+    t.integer "industry_id"
+  end
+
+  create_table "company_profiles", force: :cascade do |t|
+    t.integer "company_id"
+    t.string  "stringset_1"
+    t.string  "stringset_2"
+    t.string  "stringset_3"
+    t.string  "stringset_4"
+    t.integer "dataset_1"
+    t.integer "dataset_2"
+    t.integer "dataset_3"
+    t.integer "dataset_4"
+    t.integer "dataset_5"
+    t.integer "dataset_6"
+    t.date    "created_at"
+    t.date    "updated_at"
+  end
+
+  create_table "external_data_repositories", force: :cascade do |t|
+    t.string "category"
+    t.string "value_1"
+    t.string "value_2"
+    t.date   "created_at"
+    t.date   "updated_at"
+  end
+
+  create_table "industries", force: :cascade do |t|
+    t.string "industry_name"
+    t.date   "created_at"
+    t.date   "updated_at"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "street_address"
+    t.string "postal_code"
+    t.date   "created_at"
+    t.date   "updated_at"
+  end
+
+  create_table "results", force: :cascade do |t|
+    t.string "category"
+    t.string "value_1"
+    t.string "value_2"
+    t.date   "point_in_time"
+    t.date   "created_at"
+    t.date   "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string  "firstname"
+    t.string  "lastname"
+    t.string  "username"
+    t.string  "email"
+    t.string  "password_digest", null: false
+    t.string  "session_token"
+    t.date    "created_at"
+    t.date    "updated_at"
+    t.integer "company_id"
   end
 
 end
